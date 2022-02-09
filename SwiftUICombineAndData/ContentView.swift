e29558733939b19852234e4b1f4fb93101ccac83
Austin Burke <austinlayneburke@gmail.com>
2022-02-09 06:17:55 -0600

    Added styling around menu items

--- SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/ContentView.swift
@@ -16,8 +16,7 @@ struct ContentView: View {
                 TrackableScrollView(offsetChanged: { offset in
                     contentOffset = offset.y
                 }) {
-                    Text("Hello, world!")
-                        .padding()
+                    content
                 }
                 
                 VisualEffectBlur(blurStyle: .systemMaterial)
@@ -32,6 +31,40 @@ struct ContentView: View {
             
         }
         .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    var content: some View {
+        VStack {
+            // More content
+            VStack {
+                MenuRow()
+                divider
+                MenuRow(title: "SwiftUI Packages", leftIcon: "square.stack.3d.up.fill")
+                divider
+                MenuRow(title: "YouTube Channel", leftIcon: "play.rectangle.fill", rightIcon: "link")
+            }
+            .padding(16)
+            .background(Color("Background 1"))
+            .background(VisualEffectBlur(blurStyle: .systemUltraThinMaterialDark))
+            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.white, lineWidth: 1).blendMode(.overlay))
+            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+            .padding(.top, 20)
+            
+            Text("Version 1.00")
+                .foregroundColor(.white.opacity(0.7))
+                .padding(.top, 20)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 10)
+                .font(.footnote)
+        }
+        .foregroundColor(.white)
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .padding(.bottom, 10)
+    }
+    
+    var divider: some View {
+        Divider().background(Color.white.blendMode(.overlay))
     }
 }
 
